742656d4cff8b4ab87542a0a092941aa329c0658
Amir Mosio <amir.mn.mosio@gmail.com>
2024-03-23 19:26:08 +0100

    Intialize Project - Implement Download GroupMessage

---
+++ data_collector_client.py
@@ -0,0 +1,35 @@
+from telethon import TelegramClient, events, errors
+
+
+class AnomalyDetectionClient:
+
+    def __init__(self, phone_number, api_id, api_hash):
+        self.phone_number = phone_number
+        self.client = TelegramClient("anomaly_detection", api_id, api_hash)
+
+        @self.client.on(events.NewMessage())
+        async def handler(event):
+            if event.is_group or event.is_channel and not event.is_private:
+                pass
+            else:
+                print(event.chat_id)
+                print(event.is_group, event.is_channel, event.is_private)
+                print(event.raw_text)
+
+    async def get_messages(self, chat_id, limit):
+        async with self.client.takeout() as takeout:
+            return await takeout.get_messages(chat_id, limit=limit)  # wrapped through takeout (less limits)
+
+
+    async def connect(self, run_blocking=True):
+        await self.client.connect()
+        is_authorized = await self.client.is_user_authorized()
+        if not is_authorized:
+            await self.client.send_code_request(self.phone_number)
+            await self.client.sign_in(self.phone_number, input('Enter the code: '))
+        if run_blocking:
+            print("client started...")
+            await self.client.run_until_disconnected()
+
+    async def disconnect(self):
+        await self.client.disconnect()
---
+++ test.py
@@ -0,0 +1,27 @@
+from data_collector_client import AnomalyDetectionClient
+import asyncio
+from decouple import config
+import pandas as pd
+
+
+
+phone_number = config('TELEGRAM_CLIENT_PHONE_NUMBER')
+api_id = config('TELEGRAM_CLIENT_API_ID')
+api_hash = config('TELEGRAM_CLIENT_API_HASH')
+
+async def fetch_messages_from_client(chat):
+    client = AnomalyDetectionClient(phone_number, api_id, api_hash)
+    await client.connect(run_blocking=False)
+
+    df = pd.DataFrame()
+
+    messages = await client.get_messages(chat, 55)
+    for message in messages:
+        data = { "group" : chat, "sender" : message.sender_id, "text" : message.text, "date" : message.date}
+        temp_df = pd.DataFrame(data, index=[1])
+        df = pd.concat([df, temp_df])
+    print(df.head())
+
+test_links = {"group": "https://t.me/joinchat/BlviDUvKluDK9m3p7OpIxQ", "private": "https://t.me/AmirMosio"}
+chat = test_links['group']
+asyncio.run(fetch_messages_from_client(chat))
