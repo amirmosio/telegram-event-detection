76dc3e76ac1503960692c13820ba0f5d5c4ec7cf
Amir Mosio <amir.mn.mosio@gmail.com>
2024-04-20 13:34:19 +0200

    Correct remove link functionality

--- data_labeling/select_for_labeling.py
+++ data_labeling/select_for_labeling.py
@@ -1,15 +1,17 @@
-from data_preprocessing.remove_links import remove_links
+from data_preprocessing.remove_links import remove_links_and_empty_messages
 from data_preprocessing.translation import translate_messages
 import numpy as np
 import pandas as pd
 
 def select_data_for_labeling(df):
-    df = remove_links(df)
+    df = remove_links_and_empty_messages(df)
     # select random as there are lots of message
     new_df = pd.DataFrame()
     window_size = 300
     for group_link in df['group'].unique():
-        group_df = df.loc[df['group'] == group_link]
+        group_df = df[df['group'] == group_link]
+        group_df = group_df.sort_values(by=['date'])
+        group_df = group_df.reset_index(drop=True)
         start_idx = np.random.randint(0, len(group_df) - window_size + 1)
         group_df = group_df.iloc[start_idx : start_idx + window_size]
         group_df = group_df.drop("Unnamed: 0", axis=1)
--- data_preprocessing/remove_links.py
+++ data_preprocessing/remove_links.py
@@ -1,8 +1,8 @@
-def remove_links(df):
-    url_pattern = r'https?://\S+'
+def remove_links_and_empty_messages(df):
     df_original = df.copy()
     df_original = df_original.dropna(subset=['text']) #drop messages composed by csv files
-    rows_with_links = df_original[df_original['text'].str.contains(url_pattern)] #drop message composed by links
-    df_original = df_original.drop(rows_with_links.index)
+
+    url_pattern = r'https?://\S+'
+    df_original['text'] = df_original['text'].replace(url_pattern, '#link', regex=True)  #replcae links with 
     df_original = df_original.reset_index(drop=True) #reset index
     return df_original
