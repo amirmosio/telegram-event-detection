ff91404e3f730b81adee2f2e3fe035668d1c2398
martina viganò <martina2.vigano@mail.polimi.it>
2024-04-26 13:10:24 +0200

    Added model evaluation methods.

--- conversation_models/random_forest/model_training.py
+++ conversation_models/random_forest/model_training.py
@@ -1,6 +1,7 @@
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.metrics import accuracy_score, confusion_matrix
-
+from sklearn.metrics import confusion_matrix, accuracy_score, recall_score, precision_score
+import matplotlib.pyplot as plt
+import seaborn as sns
 
 def train_random_forest_model(X_train, y_train):
     print(f"Training on {len(y_train)} data point")
@@ -16,5 +17,20 @@ def print_model_evaluation(clf, X, y):
 
     acc = accuracy_score(y, y_pred)
     cm = confusion_matrix(y, y_pred)
-    print(acc)
-    print(cm)
+    sens = recall_score(y, y_pred)
+    prec = precision_score(y, y_pred)
+
+    print("Accuracy:", acc)
+    print("Confusion matrix:", cm)
+    print("Sensitivity:", sens)
+    print("Precision:", prec)
+
+    '''
+    Confusion matrix graph, for charts
+    plt.figure(figsize=(8, 6))
+    sns.heatmap(cm, annot=True, cmap="Greens", fmt="d", cbar=False)
+    plt.xlabel('Predicted Labels')
+    plt.ylabel('True Labels')
+    plt.title('Confusion Matrix')
+    plt.show()
+    '''
