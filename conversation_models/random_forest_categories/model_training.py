019ae9cd6c77e447a2b92c9874f2a22b0951ea5b
martina viganò <martina2.vigano@mail.polimi.it>
2024-05-14 17:30:05 +0200

    embedd message and one-hot-encoding in model_training.py

--- conversation_models/random_forest_categories/model_training.py
+++ conversation_models/random_forest_categories/model_training.py
@@ -8,9 +8,13 @@ from sklearn.metrics import (
 )
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import GridSearchCV
+from utilities import embedding_with_sentence_transformer
+import pandas as pd
+import numpy as np
 
-
-def train_random_forest_model_multiclass(X_train, y_train):
+def train_random_forest_model_multiclass(df_train, y_train):
+    X_train = embedding_with_sentence_transformer(df_train)
+    y_train = pd.get_dummies(y_train, columns = ['category'])
     best_parameters, best_model = grid_search_random_forest(X_train, y_train)
     clf = best_model
     '''
@@ -32,7 +36,7 @@ def grid_search_random_forest(X_train, y_train):
 
     clf_multiclaass = RandomForestClassifier(random_state=42)
     
-    grid_search = GridSearchCV(estimator=clf_multiclaass, param_grid=param_grid,cv=5, scoring='accuracy')######
+    grid_search = GridSearchCV(estimator=clf_multiclaass, param_grid=param_grid,cv=5, scoring='f1-score')######
 
     grid_search.fit(X_train, y_train)
 
@@ -48,9 +52,13 @@ def grid_search_random_forest(X_train, y_train):
     return best_params, best_model
 
 
-def print_model_evaluation(clf_multiclaass, X_test, y_true):
-    y_pred = clf_multiclaass.predict(X_test)
+def print_model_evaluation(clf_multiclaass, df_test, y_true):
+    X_test = embedding_with_sentence_transformer(df_test)
+    #y_true_one_hot = pd.get_dummies(y_true, columns = ['category'])
 
+    y_pred_one_hot = clf_multiclaass.predict(X_test)
+    y_pred = np.argmax(y_pred_one_hot,axis=-1)
+    
     conf_matrix = confusion_matrix(y_true, y_pred)
 
     overall_precision = precision_score(y_true, y_pred, average='macro')
